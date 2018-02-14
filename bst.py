3b295ad2661f8edf30c14fc42872f0937ebe0dbf
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-02-14 06:56:56 +0530

    bst

---
+++ bst.py
@@ -0,0 +1,22 @@
+class BST:
+    def __init__(self,value):
+        self.left = None
+        self.right = None
+        self.data = value
+
+insertIntoBST(root,value):
+    if root is None:
+        root = value
+    else:
+        if value.data < root.data:
+            if root.left is None:
+                root.left = value
+            else:
+                insertIntoBST(root.left,value)
+        else:
+            if root.right is None:
+                root.right = value
+            else:
+                insertIntoBST(root.right,value)
+
+tree = BST(7)
